7a3bed4712d8d7769932d3d6dbe0132b6c460f94
subhan <subhan97ahmed@gmail.com>
2021-10-25 20:59:39 +0500

    fixed data-ad-client and data-ad-slot attributes  in Ad.jsx

--- src/components/ad/Ad.jsx
+++ src/components/ad/Ad.jsx
@@ -10,8 +10,8 @@ class Ad extends Component {
         <div>
         <ins className = "adsbygoogle"
                 style = { {display:"inline-block",width:"728px",height:"90px"} }
-                data-ad-client = "ca-pub-114234437311277"
-                data-ad-slot = "42837282224"></ins>
+                data-ad-client = "ca-pub-5642121119628809"
+                data-ad-slot = "5642121119628809"></ins>
         </div>)
     }
 }
